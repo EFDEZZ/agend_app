55d86140d6ca224c57ad9d2a5b94e3bbc5aa6d79
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-16 01:19:09 -0400

    sliver appbar y sliverlist implementados

--- lib/src/presentation/screens/home/home_screen.dart
+++ lib/src/presentation/screens/home/home_screen.dart
@@ -7,14 +7,63 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Recordatorios"),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+      // appBar: AppBar(
+      //   title: Text("Recordatorios"),
+      //   actions: [
+      //     // IconButton(onPressed: (){}, icon: Icon(Icons.add)),
+      //     // IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+      //   ],
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            floating: false,
+            expandedHeight: 160,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: false,
+              title: Text("Recordatorios"),
+            ),
+          ),
+          SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+            
+            final appointment = appointments[index];
+            return Container(
+          margin: EdgeInsets.only(left: 5, right: 5, top: 5),
+          child: Card(
+            elevation: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(appointment.service,),
+                  Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      
+                      Text(appointment.formattedDate),
+                      // Text(appointment.formattedTime)
+                      
+                      
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+          },
+          childCount: appointments.length))
         ],
       ),
-      body: _HomeView(),
+      floatingActionButton: IconButton.filled(
+        onPressed: (){}, 
+        icon: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Icon(Icons.add, size: 35,),
+        )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
@@ -24,6 +73,7 @@ class _HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme;
     return ListView.builder(
       itemCount: appointments.length,
       itemBuilder: (context, index) {
@@ -34,10 +84,20 @@ class _HomeView extends StatelessWidget {
             elevation: 3,
             child: Padding(
               padding: const EdgeInsets.all(30.0),
-              child: Row(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(appointment.service),
-                  
+                  Text(appointment.service, style: textStyle.titleMedium),
+                  Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      
+                      Text(appointment.formattedDate),
+                      // Text(appointment.formattedTime)
+                      
+                      
+                    ],
+                  ),
                 ],
               ),
             ),
