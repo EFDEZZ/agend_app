4d01d540479dc03e26d81c141c85e7fad73e69b6
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-15 23:25:06 -0400

    appointment entity created

---
+++ lib/src/domain/entities/appointment.dart
@@ -0,0 +1,134 @@
+class Appointment {
+  final int id;
+  final String clientName;
+  final String clientPhone;
+  final DateTime date;
+  final String service;
+  final String note;
+  final bool reminderSent;
+  final DateTime createdAt;
+
+  Appointment({
+    required this.id,
+    required this.clientName,
+    required this.clientPhone,
+    required this.date,
+    required this.service,
+    required this.note,
+    required this.reminderSent,
+    required this.createdAt,
+  });
+}
+
+final appointments = <Appointment>[
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+  Appointment(
+    id: 1,
+    clientName: "Roberto",
+    clientPhone: "58490079",
+    date: DateTime(2024),
+    service: "Me pica el ano",
+    note: "Tengo que arrascarme el ano",
+    reminderSent: false,
+    createdAt: DateTime(2023),
+  ),
+];
