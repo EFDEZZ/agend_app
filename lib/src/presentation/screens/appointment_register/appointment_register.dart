9919b51787d27149a11e57f275ac2b269516538a
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-16 02:00:17 -0400

    Creado un nuevo scaffold para la pantalla de crear citas

---
+++ lib/src/presentation/screens/appointment_register/appointment_register.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppointmentRegister extends StatelessWidget {
+  const AppointmentRegister({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add your appointment"),
+      ),
+    );
+  }
+}
