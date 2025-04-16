c989f7b1650d9762c052144ad54fa17c70d4b253
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-16 01:59:56 -0400

    Customizando el homeScreen

--- lib/src/presentation/screens/home/home_screen.dart
+++ lib/src/presentation/screens/home/home_screen.dart
@@ -6,14 +6,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // final textStyle = Theme.of(context).textTheme;
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("Recordatorios"),
-      //   actions: [
-      //     // IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-      //     // IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-      //   ],
-      // ),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
@@ -21,40 +15,13 @@ class HomeScreen extends StatelessWidget {
             floating: false,
             expandedHeight: 160,
             flexibleSpace: FlexibleSpaceBar(
+              titlePadding: EdgeInsets.only(left: 20, bottom: 10),
               centerTitle: false,
-              title: Text("Recordatorios"),
+              title: Text("Recordatorios", style: TextStyle(fontFamily: "Montserrat")),
+              
             ),
           ),
-          SliverList(delegate: SliverChildBuilderDelegate((context, index) {
-            
-            final appointment = appointments[index];
-            return Container(
-          margin: EdgeInsets.only(left: 5, right: 5, top: 5),
-          child: Card(
-            elevation: 3,
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(appointment.service,),
-                  Row(
-                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      
-                      Text(appointment.formattedDate),
-                      // Text(appointment.formattedTime)
-                      
-                      
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-          },
-          childCount: appointments.length))
+          _HomeView()
         ],
       ),
       floatingActionButton: IconButton.filled(
@@ -73,37 +40,35 @@ class _HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textStyle = Theme.of(context).textTheme;
-    return ListView.builder(
-      itemCount: appointments.length,
-      itemBuilder: (context, index) {
-        final appointment = appointments[index];
-        return Container(
-          margin: EdgeInsets.only(left: 5, right: 5, top: 5),
-          child: Card(
-            elevation: 3,
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(appointment.service, style: textStyle.titleMedium),
-                  Row(
-                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      
-                      Text(appointment.formattedDate),
-                      // Text(appointment.formattedTime)
-                      
-                      
-                    ],
-                  ),
-                ],
-              ),
+    return SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+      
+      final appointment = appointments[index];
+      return Container(
+    margin: EdgeInsets.only(left: 5, right: 5, top: 5),
+    child: Card(
+      elevation: 3,
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(appointment.service,),
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                
+                Text(appointment.formattedDate),
+                // Text(appointment.formattedTime)
+                
+                
+              ],
             ),
-          ),
-        );
-      }, 
-      );
+          ],
+        ),
+      ),
+    ),
+            );
+    },
+    childCount: appointments.length));
   }
 }
