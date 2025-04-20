a6e5035f68353d9040c3889d2874ef241dcf1126
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-20 12:09:52 -0400

    barrier file updated

--- lib/src/presentation/screens/screens.dart
+++ lib/src/presentation/screens/screens.dart
@@ -1,4 +1,10 @@
+export 'package:agend_app/src/presentation/screens/auth/register_screen.dart';
+
+export 'package:agend_app/src/presentation/screens/auth/login_screen.dart';
+
 export 'package:agend_app/src/presentation/screens/appointment_register/appointment_register.dart';
 
 export 'package:agend_app/src/presentation/screens/home/home_screen.dart';
+
+
 
