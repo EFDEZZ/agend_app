c7189d39ccc87b688dd4d3ceb7ca124efb38686c
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-24 02:36:48 -0400

    customDrawer implemented

---
+++ lib/src/presentation/widgets/custom_drawer.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(color: colors.primary),
+            child: Center(
+              child: Text(
+                'Menu',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Home'),
+            onTap: () {
+              context.go('/home/');
+            },
+          ),
+          Divider(
+            indent: 20,
+            endIndent: 20,
+          ),
+          ListTile(
+            leading: const Icon(Icons.circle_notifications),
+            title: const Text('Reminders'),
+            onTap: () {
+              context.go('/reminder/');
+              // Navigate to home screen
+            },
+          ),
+          Divider(
+            indent: 20,
+            endIndent: 20,
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () {
+              context.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
