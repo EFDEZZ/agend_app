c97f837f21a0c0ca3d6214b5b0eaca3493a5ff68
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-24 02:36:20 -0400

    basic homeScreen for testing

--- lib/src/presentation/screens/home/home_screen.dart
+++ lib/src/presentation/screens/home/home_screen.dart
@@ -1,5 +1,4 @@
-import 'package:agend_app/src/domain/entities/appointment.dart';
-import 'package:agend_app/src/presentation/screens/screens.dart';
+import 'package:agend_app/src/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,82 +6,14 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final textStyle = Theme.of(context).textTheme;
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      drawer: CustomDrawer(),
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            pinned: true,
-            floating: false,
-            expandedHeight: 160,
-            flexibleSpace: FlexibleSpaceBar(
-              titlePadding: EdgeInsets.only(left: 20, bottom: 10),
-              centerTitle: false,
-              title: Text("Recordatorios", style: TextStyle(fontFamily: "Montserrat")),
-              
-            ),
-          ),
-          _HomeView(),
-        ],
+          CustomSliverAppbar(title: 'HOME'), 
+          // Text('Home Screen')
+          ],
       ),
-      floatingActionButton: IconButton.filled(
-        onPressed: (){
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context, 
-            builder: (context) {
-              return AppointmentRegister();
-            },
-            );
-        }, 
-        icon: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Icon(Icons.add, size: 35,),
-        )),
-      
     );
-  }
-}
-
-class _HomeView extends StatelessWidget {
-  const _HomeView();
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverList(delegate: SliverChildBuilderDelegate((context, index) {
-      
-      final appointment = appointments[index];
-      return Container(
-    margin: EdgeInsets.only(left: 5, right: 5, top: 5),
-    child: Card(
-      elevation: 3,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(15),
-        onTap: (){},
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(appointment.service,),
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  
-                  Text(appointment.formattedDate),
-                  // Text(appointment.formattedTime)
-                  
-                  
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-            );
-    },
-    childCount: appointments.length));
   }
 }
