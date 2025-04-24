803f609d132eaea109ad35829db5bdff915cec2e
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-24 02:34:53 -0400

    cuostmized sliverAppBar implemented

---
+++ lib/src/presentation/widgets/custom_sliver_appbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomSliverAppbar extends StatelessWidget {
+  final String title;
+  const CustomSliverAppbar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return SliverAppBar(
+            centerTitle: true,
+            pinned: true,
+            floating: false,
+            expandedHeight: 160,
+            flexibleSpace: FlexibleSpaceBar(
+              titlePadding: EdgeInsets.only(left: 0, bottom: 15,),
+              centerTitle: true,
+              title: Text(
+                title,
+                style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
+  }
+}
