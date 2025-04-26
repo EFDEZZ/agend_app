ee24a1579e63d62abd5b565cbaf2ea90e4277b10
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-25 20:58:01 -0400

    barrier file services updated

--- lib/src/infrastructure/services/services.dart
+++ lib/src/infrastructure/services/services.dart
@@ -2,7 +2,7 @@ export 'package:agend_app/src/infrastructure/services/appointment_services/appoi
 
 export 'package:agend_app/src/infrastructure/services/auth_services/auth_storage.dart';
 
-export 'package:agend_app/src/infrastructure/services/appointment_services/register_appointment_service.dart';
+export 'package:agend_app/src/infrastructure/services/appointment_services/appointment_register_service.dart';
 
 export 'package:agend_app/src/infrastructure/services/auth_services/login_service.dart';
 
