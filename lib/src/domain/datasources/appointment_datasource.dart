b4e1bd6a130d55e6e0a38253f02101680d18a2d4
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-27 02:37:37 -0400

    created datasource and repository for the domain layer

---
+++ lib/src/domain/datasources/appointment_datasource.dart
@@ -0,0 +1,9 @@
+
+import 'package:agend_app/src/domain/entities/appointment.dart';
+
+abstract class AppointmentDatasource {
+  Future<bool> createAppointment({required DateTime date, required String service, required String notes});
+  Future<List<Appointment>> getAppointmentsByUser();
+  Future<List<Appointment>> getAllAppointments();
+  Future<void> deleteAppointment(int appointmentId);
+}
---
+++ lib/src/domain/repositories/appointment_repository.dart
@@ -0,0 +1,9 @@
+
+import 'package:agend_app/src/domain/entities/appointment.dart';
+
+abstract class AppointmentRepository {
+  Future<bool> createAppointment({required DateTime date, required String service, required String notes});
+  Future<List<Appointment>> getAppointmentsByUser();
+  Future<List<Appointment>> getAllAppointments();
+  Future<void> deleteAppointment(int appointmentId);
+}
