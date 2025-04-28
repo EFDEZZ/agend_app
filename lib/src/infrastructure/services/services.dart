316b76082e1f19e6a7a6e5ef2b14a10456cce1c5
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-27 23:53:46 -0400

    changes for a best clean architecture

--- lib/src/infrastructure/services/services.dart
+++ lib/src/infrastructure/services/services.dart
@@ -1,8 +1,6 @@
 
 export 'package:agend_app/src/infrastructure/services/auth_services/auth_storage.dart';
 
-export 'package:agend_app/src/infrastructure/services/appointment_services/create_appointment_service.dart';
-
 export 'package:agend_app/src/infrastructure/services/auth_services/login_service.dart';
 
 export 'package:agend_app/src/infrastructure/services/auth_services/register_service.dart';
