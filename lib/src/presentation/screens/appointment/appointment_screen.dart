17be40eb86be9d1318862c03ba5db00b78742b70
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-28 01:25:51 -0400

    code optimization

--- lib/src/presentation/screens/appointment/appointment_screen.dart
+++ lib/src/presentation/screens/appointment/appointment_screen.dart
@@ -1,9 +1,9 @@
+import 'package:agend_app/src/domain/entities/appointment.dart';
 import 'package:agend_app/src/presentation/screens/screens.dart';
 import 'package:agend_app/src/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:agend_app/src/infrastructure/providers/appointments_provider.dart';
-import 'package:go_router/go_router.dart';
 
 class AppointmentScreen extends ConsumerWidget {
   const AppointmentScreen({super.key});
@@ -27,147 +27,7 @@ class AppointmentScreen extends ConsumerWidget {
                   sliver: SliverList(
                     delegate: SliverChildBuilderDelegate((context, index) {
                       final appointment = appointments[index];
-                      return Container(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 1,
-                          vertical: 3,
-                        ),
-                        child: SizedBox(
-                          height: 150,
-                          child: Card(
-                            elevation: 3,
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(15),
-                              onTap: () {
-                                //TODO: Agregar funcionalidad AppointmentDetailsView
-                              },
-                              child: Padding(
-                                padding: EdgeInsets.only(
-                                  top: 20,
-                                  bottom: 20,
-                                  left: 15,
-                                  right: 5,
-                                ),
-                                child: Row(
-                                  children: [
-                                    Expanded(
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            appointment.service,
-                                            style: const TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                            maxLines: 1,
-                                            overflow: TextOverflow.ellipsis,
-                                          ),
-                                          const SizedBox(height: 5),
-                                          Text(
-                                            appointment.note,
-                                            maxLines: 2,
-                                            overflow: TextOverflow.ellipsis,
-                                          ),
-                                          const SizedBox(height: 10),
-                                          Text(
-                                            "Date: ${appointment.formattedDate}",
-                                            style: TextStyle(
-                                              color: const Color.fromARGB(255, 203, 107, 12),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                  
-                                    // Buttons
-                                    IconButton(
-                                      onPressed: () {
-                                        // Aquí agregarías la funcionalidad para enviar recordatorios
-                                      },
-                                      icon: Icon(
-                                        Icons.notifications_none_outlined,
-                                        color: colors.primary,
-                                      ),
-                                    ),
-                                    IconButton(
-                                      onPressed: () async {
-                                        // Confirmación de eliminación
-                                        final confirmDelete = await showDialog<
-                                          bool
-                                        >(
-                                          context: context,
-                                          builder: (context) {
-                                            return AlertDialog(
-                                              title: const Text(
-                                                'Delete Appointment',
-                                              ),
-                                              content: const Text(
-                                                'Are you sure you want to delete this appointment?',
-                                              ),
-                                              actions: <Widget>[
-                                                TextButton(
-                                                  onPressed:
-                                                      () => Navigator.pop(
-                                                        context,
-                                                        false,
-                                                      ),
-                                                  child: const Text('Cancel'),
-                                                ),
-                                                TextButton(
-                                                  onPressed:
-                                                      () => Navigator.pop(
-                                                        context,
-                                                        true,
-                                                      ),
-                                                  child: const Text(
-                                                    'Delete',
-                                                    style: TextStyle(
-                                                      color: Color.fromARGB(
-                                                        255,
-                                                        175,
-                                                        21,
-                                                        10,
-                                                      ),
-                                                    ),
-                                                  ),
-                                                ),
-                                              ],
-                                            );
-                                          },
-                                        );
-                  
-                                        if (confirmDelete == true) {
-                                          // Llamar para eliminar
-                                          await ref
-                                              .read(
-                                                appointmentDeleteStateNotifierProvider
-                                                    .notifier,
-                                              )
-                                              .deleteAppointment(appointment.id);
-                  
-                                          // Invalidar para refrescar la lista
-                                          ref.invalidate(
-                                            appointmentsProvider,
-                                          );
-                  
-                                        
-                                        }
-                                      },
-                                      icon: const Icon(
-                                        Icons.delete_forever_outlined,
-                                        color: Color.fromARGB(255, 188, 24, 12),
-                                        size: 30,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
+                      return _CustomAppointmentCard(appointment: appointment, colors: colors);
                     }, childCount: appointments.length),
                   ),
                 ),
@@ -183,6 +43,159 @@ class AppointmentScreen extends ConsumerWidget {
         ],
       ),
     );
+  }
+}
+
+class _CustomAppointmentCard extends ConsumerWidget {
+  const _CustomAppointmentCard({
+    required this.appointment,
+    required this.colors,
+  });
+
+  final Appointment appointment;
+  final ColorScheme colors;
+
+  @override
+  Widget build(BuildContext context, ref) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 1,
+        vertical: 3,
+      ),
+      child: SizedBox(
+        height: 150,
+        child: Card(
+          elevation: 3,
+          child: InkWell(
+            borderRadius: BorderRadius.circular(15),
+            onTap: () {
+              //TODO: Agregar funcionalidad AppointmentDetailsView
+            },
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: 20,
+                bottom: 20,
+                left: 15,
+                right: 5,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment:
+                          CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          appointment.service,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          appointment.note,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          "Date: ${appointment.formattedDate}",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 203, 107, 12),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                      
+                  // Buttons
+
+                  //Notification Button
+                  IconButton(
+                    onPressed: () {
+                      // Aquí agregarías la funcionalidad para enviar recordatorios
+                    },
+                    icon: Icon(
+                      Icons.notifications_none_outlined,
+                      color: colors.primary,
+                    ),
+                  ),
+                  //Delete Button
+                  IconButton(
+                    onPressed: () async {
+                      // Confirmación de eliminación
+                      final confirmDelete = await _showDeleteConfirmationDialog(context);
+                      if (confirmDelete == true) {
+                        // Llamar para eliminar
+                        await ref.read(appointmentDeleteStateNotifierProvider.notifier)
+                            .deleteAppointment(appointment.id);
+                        // Invalidar para refrescar la lista
+                        ref.invalidate(appointmentsProvider);
+                      }
+                    },
+                    icon: const Icon(
+                      Icons.delete_forever_outlined,
+                      color: Color.fromARGB(255, 188, 24, 12),
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Future<bool?> _showDeleteConfirmationDialog(BuildContext context) {
+    return showDialog<
+                      bool
+                    >(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text(
+                            'Delete Appointment',
+                          ),
+                          content: const Text(
+                            'Are you sure you want to delete this appointment?',
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed:
+                                  () => Navigator.pop(
+                                    context,
+                                    false,
+                                  ),
+                              child: const Text('Cancel'),
+                            ),
+                            TextButton(
+                              onPressed:
+                                  () => Navigator.pop(
+                                    context,
+                                    true,
+                                  ),
+                              child: const Text(
+                                'Delete',
+                                style: TextStyle(
+                                  color: Color.fromARGB(
+                                    255,
+                                    175,
+                                    21,
+                                    10,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    );
   }
 }
 
