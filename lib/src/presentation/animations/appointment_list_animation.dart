d167cd9272023f6d0c598894ddad9e2f46973f5e
EDRIEN FDEZ <edriennn2001@gmail.com>
2025-04-29 02:51:12 -0400

    animations for the appointmentList

---
+++ lib/src/presentation/animations/appointment_list_animation.dart
@@ -0,0 +1,95 @@
+// appointment_animations.dart
+import 'package:agend_app/src/presentation/screens/screens.dart';
+import 'package:flutter/material.dart';
+import 'package:agend_app/src/domain/entities/appointment.dart';
+
+class AppointmentListAnimation extends StatefulWidget {
+  final List<Appointment> appointments;
+
+  const AppointmentListAnimation({super.key, required this.appointments});
+
+  @override
+  State<AppointmentListAnimation> createState() => _AppointmentListAnimationState();
+}
+
+class _AppointmentListAnimationState extends State<AppointmentListAnimation> {
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+  late List<Appointment> _items;
+
+  @override
+  void initState() {
+    super.initState();
+    _items = [];
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      for (int i = 0; i < widget.appointments.length; i++) {
+        _items.add(widget.appointments[i]);
+        _listKey.currentState?.insertItem(
+          i,
+          duration: const Duration(milliseconds: 300),
+        );
+      }
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant AppointmentListAnimation oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    oldWidget.appointments.map((e) => e.id).toSet();
+    final newIds = widget.appointments.map((e) => e.id).toSet();
+
+    for (int i = 0; i < _items.length; i++) {
+      if (!newIds.contains(_items[i].id)) {
+        final removedItem = _items.removeAt(i);
+        _listKey.currentState?.removeItem(
+          i,
+          (context, animation) => SizeTransition(
+            sizeFactor: animation,
+            child: CustomAppointmentCard(
+              appointment: removedItem,
+              colors: Theme.of(context).colorScheme,
+            ),
+          ),
+          duration: const Duration(milliseconds: 300),
+        );
+        break;
+      }
+    }
+
+    for (int i = 0; i < widget.appointments.length; i++) {
+      if (!_items.any((a) => a.id == widget.appointments[i].id)) {
+        _items.insert(i, widget.appointments[i]);
+        _listKey.currentState?.insertItem(
+          i,
+          duration: const Duration(milliseconds: 300),
+        );
+        break;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 100),
+      child: AnimatedList(
+        key: _listKey,
+        initialItemCount: _items.length,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index, animation) {
+          final appointment = _items[index];
+          return SizeTransition(
+            sizeFactor: animation,
+            child: CustomAppointmentCard(
+              appointment: appointment,
+              colors: colors,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
